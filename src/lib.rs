2d30ca5d3466434e6d355d5108b979a7977ca76b
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2025-10-21 06:01:02 +0400

    net print

--- src/lib.rs
+++ src/lib.rs
@@ -1910,8 +1910,14 @@ pub async fn entry_point(my_root_private_key: SigningKey, my_static_keypair: Opt
                     let  pps = (std::cmp::max(1, net_stats.packets_sent) as f32)          / (nonzero_elapsed_sec);
                     let  bpp = (std::cmp::max(1, net_stats.bytes_sent)   as f32)          / (std::cmp::max(1, net_stats.packets_sent) as f32);
 
-                    println!("\x1b[92mNET\x1b[0m: {} Kb/s | {} packets/s | {} bytes/packet",
-                             kbps as u32, pps as u32, bpp as u32);
+                    let kbps = kbps as u32;
+                    let  pps =  pps as u32;
+                    let  bpp =  bpp as u32;
+
+                    if kbps != 0 {
+                        println!("{}: \x1b[92mNET\x1b[0m: {} Kb/s | {} packets/s | {} bytes/packet",
+                                 ctx_str, kbps, pps, bpp);
+                    }
                 }
 
                 break;
